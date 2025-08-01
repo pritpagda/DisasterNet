4ac09b71d781fc7368502108b72fb28184bf4743
Prit Pagda <pritpagda@gmail.com>
2025-08-01 15:28:50 -0400

    Final yaml file

--- server/backend/app/db.py
+++ server/backend/app/db.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://devuser:devpass@db:5432/devdb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
